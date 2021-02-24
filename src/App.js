f566975e8add37af66ea4e1f8b8ea21d0c7a0ae2
Raul Romo <rromomxdev@gmail.com>
2021-02-23 19:23:25 -0600

    init: repo initialization and first tests with CSS and user renderization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useEffect, useState} from 'react'
+import './App.css'
+
+import * as csstree from 'css-tree'
+import * as cssom from 'cssom'
+
+import stringify from 'json-stringify-safe'
 
 function App() {
+
+  // const [cssInputTest, setCssInputTest] = useState(`
+  // body {
+  //   background: #eee;
+  //   color: #888;
+  // }
+  // `)
+
+  const [cssInputTest, setCssInputTest] = useState(`
+  <style>  
+body {
+    background: #eee;
+    color: #888;
+  }
+  
+ .clase2 {
+    background: #FF00FF;
+    color: #888;
+  }
+  
+</style>
+<div class='clase2'>
+text de prueba
+</div>
+  `)
+  const userInput = useRef()
+  
+  const getInput = function() {
+    const datos = userInput.current.value.trim();
+    
+    const ast = csstree.parse(datos, {
+        positions: true,
+        tolerant: true
+    })
+    console.log(csstree.toPlainObject(ast))
+    //console.log(JSON.stringify(csstree.toPlainObject(ast)))
+
+    const astcssom = cssom.parse(datos)
+    console.log(astcssom)
+  }
+
+  function createMarkup() {
+    return {__html: cssInputTest};
+  }
+
+  useEffect(() => {
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="nav">
+        Hello Nav
+      </div>
+      <div className="description">
+        Description
+        <button onClick={() => getInput()}>Parse</button>
+        <button>Validate</button>
+      </div>
+      <div>Editor</div>
+      <div>Parser</div>
+      <div>Box1</div>
+      <div>Box2</div>
+      <textarea 
+        ref={userInput} 
+        value={cssInputTest}
+        onChange={value => setCssInputTest(value.target.value)}
+        style={{width:'250px', height:'500px'}}
         >
-          Learn React
-        </a>
-      </header>
+
+        </textarea>
+      <div></div>
+      <div style={{ width: '500px', height: '200px'}} dangerouslySetInnerHTML={createMarkup()}></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
